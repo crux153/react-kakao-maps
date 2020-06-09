1d290596c185bd81da939c08e1f9dcff52d460de
Crux <crux153@gmail.com>
2020-06-09 17:32:23 +0900

    Optimize MarkerClusterer

--- src/MarkerClusterer.tsx
+++ src/MarkerClusterer.tsx
@@ -2,7 +2,12 @@ import React from "react";
 
 import { MapContext } from "./Map";
 
-export const MarkerClustererContext = React.createContext<kakao.maps.MarkerClusterer | null>(
+interface MarkerClustererContextValue {
+  addMarker(marker: kakao.maps.Marker): void;
+  removeMarker(marker: kakao.maps.Marker): void;
+}
+
+export const MarkerClustererContext = React.createContext<MarkerClustererContextValue | null>(
   null
 );
 
@@ -15,11 +20,21 @@ export class MarkerClusterer extends React.PureComponent<MarkerClustererProps> {
   public context!: React.ContextType<typeof MapContext>;
 
   private readonly markerClusterer: kakao.maps.MarkerClusterer;
+  private readonly contextValue: MarkerClustererContextValue;
+
+  private addMarkerQueue: kakao.maps.Marker[] = [];
+  private removeMarkerQueue: kakao.maps.Marker[] = [];
+
+  private addMarkerTimeout: number | null = null;
+  private removeMarkerTimeout: number | null = null;
 
   constructor(props: MarkerClustererProps) {
     super(props);
-    const { options } = props;
-    this.markerClusterer = new kakao.maps.MarkerClusterer(options);
+    this.markerClusterer = new kakao.maps.MarkerClusterer(props.options);
+    this.contextValue = {
+      addMarker: this.addMarker.bind(this),
+      removeMarker: this.removeMarker.bind(this)
+    };
   }
 
   public componentDidMount() {
@@ -34,9 +49,33 @@ export class MarkerClusterer extends React.PureComponent<MarkerClustererProps> {
 
   public render() {
     return (
-      <MarkerClustererContext.Provider value={this.markerClusterer}>
+      <MarkerClustererContext.Provider value={this.contextValue}>
         {this.props.children}
       </MarkerClustererContext.Provider>
     );
+  }
+
+  private addMarker(marker: kakao.maps.Marker) {
+    this.addMarkerQueue.push(marker);
+    if (this.addMarkerTimeout) {
+      clearTimeout(this.addMarkerTimeout);
+      this.addMarkerTimeout = null;
+    }
+    this.addMarkerTimeout = setTimeout(() => {
+      this.markerClusterer.addMarkers(this.addMarkerQueue);
+      this.addMarkerQueue = [];
+    }, 10);
+  }
+
+  private removeMarker(marker: kakao.maps.Marker) {
+    this.removeMarkerQueue.push(marker);
+    if (this.removeMarkerTimeout) {
+      clearTimeout(this.removeMarkerTimeout);
+      this.removeMarkerTimeout = null;
+    }
+    this.removeMarkerTimeout = setTimeout(() => {
+      this.markerClusterer.removeMarkers(this.removeMarkerQueue);
+      this.removeMarkerQueue = [];
+    }, 10);
   }
 }
