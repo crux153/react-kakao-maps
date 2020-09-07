1c87d8c9c9d9df59c6c33dbfafa7bf70c3736d88
Crux <crux153@gmail.com>
2020-09-07 16:03:09 +0900

    Enforce react related eslint rules

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useMemo } from "react";
+import React, { useState, useContext, useEffect } from "react";
 
 import { MapContext } from "./Map";
 import { MarkerClustererContext } from "./MarkerClusterer";
@@ -10,130 +10,129 @@ export interface MarkerProps {
   onMouseOut?(event: any): void;
 }
 
-export const Marker = React.memo(
-  ({ options, onClick, onMouseOver, onMouseOut }: MarkerProps) => {
-    const marker = useMemo(() => new kakao.maps.Marker(options), []);
+export const Marker = React.memo(function Marker({
+  options,
+  onClick,
+  onMouseOver,
+  onMouseOut,
+}: MarkerProps) {
+  const marker = useState(() => new kakao.maps.Marker(options))[0];
 
-    const map = useContext(MapContext);
-    const clusterer = useContext(MarkerClustererContext);
+  const map = useContext(MapContext);
+  const clusterer = useContext(MarkerClustererContext);
 
-    useEffect(() => {
+  useEffect(() => {
+    if (clusterer) {
+      clusterer.addMarker(marker);
+    } else {
+      marker.setMap(map);
+    }
+    return () => {
       if (clusterer) {
-        clusterer.addMarker(marker);
+        clusterer.removeMarker(marker);
       } else {
-        marker.setMap(map);
+        marker.setMap(null);
       }
+    };
+  }, [marker, map, clusterer]);
+
+  useEffect(() => {
+    if (onClick) {
+      kakao.maps.event.addListener(marker, MarkerEvent.click, onClick);
       return () => {
-        if (clusterer) {
-          clusterer.removeMarker(marker);
-        } else {
-          marker.setMap(null);
-        }
+        kakao.maps.event.removeListener(marker, MarkerEvent.click, onClick);
       };
-    }, [map, clusterer]);
+    }
+  }, [marker, onClick]);
 
-    useEffect(() => {
-      if (onClick) {
-        kakao.maps.event.addListener(marker, MarkerEvent.click, onClick);
-        return () => {
-          kakao.maps.event.removeListener(marker, MarkerEvent.click, onClick);
-        };
-      }
-    }, [marker, onClick]);
-
-    useEffect(() => {
-      if (onMouseOver) {
-        kakao.maps.event.addListener(
+  useEffect(() => {
+    if (onMouseOver) {
+      kakao.maps.event.addListener(marker, MarkerEvent.mouseover, onMouseOver);
+      return () => {
+        kakao.maps.event.removeListener(
           marker,
           MarkerEvent.mouseover,
           onMouseOver
         );
-        return () => {
-          kakao.maps.event.removeListener(
-            marker,
-            MarkerEvent.mouseover,
-            onMouseOver
-          );
-        };
-      }
-    }, [marker, onMouseOver]);
+      };
+    }
+  }, [marker, onMouseOver]);
 
-    useEffect(() => {
-      if (onMouseOut) {
-        kakao.maps.event.addListener(marker, MarkerEvent.mouseout, onMouseOut);
-        return () => {
-          kakao.maps.event.removeListener(
-            marker,
-            MarkerEvent.mouseout,
-            onMouseOut
-          );
-        };
-      }
-    }, [marker, onMouseOut]);
+  useEffect(() => {
+    if (onMouseOut) {
+      kakao.maps.event.addListener(marker, MarkerEvent.mouseout, onMouseOut);
+      return () => {
+        kakao.maps.event.removeListener(
+          marker,
+          MarkerEvent.mouseout,
+          onMouseOut
+        );
+      };
+    }
+  }, [marker, onMouseOut]);
 
-    useEffect(() => {
-      if (typeof options.altitude !== "undefined") {
-        marker.setAltitude(options.altitude);
-      }
-    }, [marker, options.altitude]);
+  useEffect(() => {
+    if (typeof options.altitude !== "undefined") {
+      marker.setAltitude(options.altitude);
+    }
+  }, [marker, options.altitude]);
 
-    useEffect(() => {
-      if (typeof options.clickable !== "undefined") {
-        marker.setClickable(options.clickable);
-      }
-    }, [marker, options.clickable]);
+  useEffect(() => {
+    if (typeof options.clickable !== "undefined") {
+      marker.setClickable(options.clickable);
+    }
+  }, [marker, options.clickable]);
 
-    useEffect(() => {
-      if (typeof options.draggable !== "undefined") {
-        marker.setDraggable(options.draggable);
-      }
-    }, [marker, options.draggable]);
+  useEffect(() => {
+    if (typeof options.draggable !== "undefined") {
+      marker.setDraggable(options.draggable);
+    }
+  }, [marker, options.draggable]);
 
-    useEffect(() => {
-      if (typeof options.image !== "undefined") {
-        marker.setImage(options.image);
-      }
-    }, [marker, options.image]);
+  useEffect(() => {
+    if (typeof options.image !== "undefined") {
+      marker.setImage(options.image);
+    }
+  }, [marker, options.image]);
 
-    useEffect(() => {
-      if (typeof options.map !== "undefined") {
-        marker.setMap(options.map);
-      }
-    }, [marker, options.map]);
+  useEffect(() => {
+    if (typeof options.map !== "undefined") {
+      marker.setMap(options.map);
+    }
+  }, [marker, options.map]);
 
-    useEffect(() => {
-      if (typeof options.opacity !== "undefined") {
-        marker.setOpacity(options.opacity);
-      }
-    }, [marker, options.opacity]);
+  useEffect(() => {
+    if (typeof options.opacity !== "undefined") {
+      marker.setOpacity(options.opacity);
+    }
+  }, [marker, options.opacity]);
 
-    useEffect(() => {
-      if (typeof options.position !== "undefined") {
-        marker.setPosition(options.position);
-      }
-    }, [marker, options.position]);
+  useEffect(() => {
+    if (typeof options.position !== "undefined") {
+      marker.setPosition(options.position);
+    }
+  }, [marker, options.position]);
 
-    useEffect(() => {
-      if (typeof options.range !== "undefined") {
-        marker.setRange(options.range);
-      }
-    }, [marker, options.range]);
+  useEffect(() => {
+    if (typeof options.range !== "undefined") {
+      marker.setRange(options.range);
+    }
+  }, [marker, options.range]);
 
-    useEffect(() => {
-      if (typeof options.title !== "undefined") {
-        marker.setTitle(options.title);
-      }
-    }, [marker, options.title]);
+  useEffect(() => {
+    if (typeof options.title !== "undefined") {
+      marker.setTitle(options.title);
+    }
+  }, [marker, options.title]);
 
-    useEffect(() => {
-      if (typeof options.zIndex !== "undefined") {
-        marker.setZIndex(options.zIndex);
-      }
-    }, [marker, options.zIndex]);
+  useEffect(() => {
+    if (typeof options.zIndex !== "undefined") {
+      marker.setZIndex(options.zIndex);
+    }
+  }, [marker, options.zIndex]);
 
-    return null;
-  }
-);
+  return null;
+});
 
 enum MarkerEvent {
   click = "click",
